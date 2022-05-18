1a2591750cbb91618437146f7b419ec22adbe381
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-05-18 18:57:25 +0300

    Step 1: fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
