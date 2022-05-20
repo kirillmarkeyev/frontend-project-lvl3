27a0dabd5481ca2d5549c713c2fad9eac5ae9a5d
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-05-20 20:59:43 +0300

    Step 3: add index.js and ru.js to src/locales

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    errors: {
+      url: 'Ссылка должна быть валидным URL',
+      notOneOf: 'RSS уже существует',
+    },
+    success: {
+      rss: 'RSS успешно загружен',
+    },
+  },
+};
