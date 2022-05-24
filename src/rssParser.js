991cd23127bbb0eb7ee992955156701b2190d651
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-05-24 21:00:00 +0300

    Step 4: add rssParser.js

---
+++ src/rssParser.js
@@ -0,0 +1,30 @@
+const getParsedRSS = (content) => {
+  const parser = new DOMParser();
+  const parsedContent = parser.parseFromString(content, 'application/xml');
+
+  if (parsedContent.querySelector('parsererror')) {
+    throw new Error('notValidRss');
+  }
+
+  const titleEl = parsedContent.querySelector('title');
+  const title = titleEl.textContent;
+  const descriptionEl = parsedContent.querySelector('description');
+  const description = descriptionEl.textContent;
+
+  const items = parsedContent.querySelectorAll('item');
+  const posts = [...items].map((item) => {
+    const itemTitleEl = item.querySelector('title');
+    const itemTitle = itemTitleEl.textContent;
+
+    const itemDescriptionEl = item.querySelector('description');
+    const itemDescription = itemDescriptionEl.textContent;
+
+    const itemLinkEl = item.querySelector('link');
+    const itemLink = itemLinkEl.textContent;
+
+    return { title: itemTitle, description: itemDescription, link: itemLink };
+  });
+  return { title, description, posts };
+};
+
+export default getParsedRSS;
