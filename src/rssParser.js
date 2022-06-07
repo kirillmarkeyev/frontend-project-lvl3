a0832a7cc71b7ed33986103a46b1131ea3589763
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-06-08 00:25:27 +0300

    Code-review 4: update parsing error

--- src/rssParser.js
+++ src/rssParser.js
@@ -9,7 +9,6 @@ const getParsedRSS = (content, url = null) => {
     error.name = 'ParsingError';
     error.message = 'Does not contain valid RSS';
     error.isParsingError = true;
-    console.log(error.message);
     throw error;
   }
 
